28fd0efa02ca51f1e2949c15c2be76bc54774832
muhammadfahrezarahman-code <anwarfahreza4@gmail.com>
2026-02-23 11:15:30 +0700

    membuat program inti/main

---
+++ Pemdas.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //file yang disertakan
+//using namespace std;
+using std::cout;
+using std::endl;
+
+int main() //program utama
+{
+    //menampilkan string
+    cout << "selamat datang di TI UMY" << endl;
+    //Menampilkan numeris
+    cout << 10;
+}
